226bc33202324b41b6ff2d30f7c488c876b3a468
Adithyan Anil <adithyananil2002@gmail.com>
2023-12-20 11:09:59 +0530

    update 1.6.1 added 404 page

---
+++ src/PageNoteFound.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function PageNoteFound() {
+  return (
+    <div className="max-w-[50rem] flex flex-col mx-auto w-full h-full">
+  <header className="mb-auto flex justify-center z-50 w-full py-4">
+    
+  </header>
+  <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+    <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">
+      404
+    </h1>
+    <h1 className="block text-2xl font-bold text-white" />
+    <p className="mt-3 text-gray-600 dark:text-gray-400">
+      Oops, something went wrong.
+    </p>
+    <p className="text-gray-600 dark:text-gray-400">
+      Sorry, we couldn't find your page.
+    </p>
+    <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+
+    </div>
+  </div>
+  
+</div>
+
+  )
+}
+
+export default PageNoteFound
